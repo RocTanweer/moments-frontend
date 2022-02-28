791753e8a8e9f058c0b0c7e1de5bb39d580c868a
RocTanweer <mohammadtanweer769@gmail.com>
2022-02-28 15:49:31 +0530

    intialized next tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkmode: false,
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
