b68fd1f8e8a1f9e802ff457ed5b74266e07ca783
RocTanweer <mohammadtanweer769@gmail.com>
2022-03-04 11:16:02 +0530

    created input component(incomplete)

---
+++ components/input/Input.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Input({ type, name }) {
+  return (
+    <div className="relative mb-1 w-full">
+      <input
+        type={type}
+        name={name}
+        id={name}
+        className="peer border-0 border-b-2 w-full border-gray-300 text-gray-900 focus:border-rose-600 focus:outline-none placeholder-transparent"
+        autoComplete="off"
+        placeholder={`Enter ${name}`}
+      />
+      <lable
+        htmlFor={name}
+        className="absolute pointer-events-none left-0 peer-placeholder-shown:top-1 peer-placeholder-shown:text-md peer-placeholder-shown:text-gray-900 peer-focus:-top-4 peer-focus:text-sm peer-focus:text-gray-500 -top-4 text-sm text-gray-500  transition-all"
+      >
+        {name}
+      </lable>
+    </div>
+  );
+}
+
+export default Input;
